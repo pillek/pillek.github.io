36f5f16f38effba08f130fcfc6c3a28bb44c25e4
Philipp Kreis <75421040+pillek@users.noreply.github.com>
2024-05-21 20:41:29 +0200

    Dashboard hinzufügen

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
