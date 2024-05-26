a6f1d8428a80a2f1435fdb8197476a91988442a4
Philipp Kreis <75421040+pillek@users.noreply.github.com>
2024-05-26 18:20:35 +0200

    dashboard

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/dashboard')
+@app.route('/login')
+def index():
+    return render_template('login.html')
+
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        preferences = request.form.get('preferences')
+        biography = request.form.get('biography')
+        return 'Erfolgreich abgesendet!'
     return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
