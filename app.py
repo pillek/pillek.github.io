869ff0135f6d32ddf0d4c92609b5f29134bcbe5b
Philipp Kreis <75421040+pillek@users.noreply.github.com>
2024-05-26 18:31:53 +0200

    login seite

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/login')
-def index():
+def login():
     return render_template('login.html')
 
 @app.route('/dashboard', methods=['GET', 'POST'])
