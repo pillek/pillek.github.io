e8e1e2188dfff82f78c16683b3a123ce3a1110cf
Philipp Kreis <75421040+pillek@users.noreply.github.com>
2024-07-18 13:18:59 +0200

    RegistrationForm und LoginForm

--- forms.py
+++ forms.py
@@ -3,15 +3,14 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    usermail = StringField('Usermail', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     name = StringField('Name')
-    description = TextAreaField('Description')
+    description = StringField('Description')
     image = StringField('Image URL')
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    usermail = StringField('Usermail', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
