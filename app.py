2e0e5cb76420aa31021f8a96201d0fdda5b33386
Philipp Kreis <75421040+pillek@users.noreply.github.com>
2024-07-24 09:21:01 +0200

    Zusammenführung unseres offiziellen Projektes und Raslans lokalem Projekt, da bei ihm Flask auf dem Rechner nicht funktioniert

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 from forms import RegistrationForm, LoginForm
 import os 
+from mailjet_rest import Client
+
+api_key = '0ea29243b0002891e017d12b70a4bbda'
+api_secret = 'e83685e4f462e8cf40097d9b895ac6a1'
+mailjet = Client(auth=(api_key, api_secret))
 
 app = Flask(__name__)
 mail = Mail(app)
@@ -45,11 +50,7 @@ def overview():
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    # if request.method == 'POST':
-    #     name = request.form.get('name')
-    #     preferences = request.form.get('preferences')
-    #     biography = request.form.get('biography')
-    #     return 'Erfolgreich abgesendet!'
+
      return render_template('dashboard.html')
 
 @app.route('/home',methods=['GET', 'POST'])
@@ -70,21 +71,41 @@ def send_email():
     subject = data['subject']
     message_body = data['message']
 
-    msg = Message(
-        subject,
-        sender='tippitopp034@gmail.com',
-        recipients=[email]
-    )
-    msg.body = message_body
+    data = {
+        'FromEmail': 'tippitopp034@gmail.com',
+        'FromName': 'Fashion Website',
+        'Subject': subject + ' - ' + email,
+        'Text-part': message_body,
+        'Html-part': message_body,
+        'Recipients': [{'Email': 'tippitopp034@gmail.com'}]
+    }
 
     try:
-        mail.send(msg)
+        result = mailjet.send.create(data=data)
+        print(result.status_code)
+        print(result.json())
         return jsonify({"status": "success", "message": "Email sent successfully!"}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
 
+@app.route('/process_payment', methods=['POST'])
+def process_payment():
+    data = request.json
+    print("Received payment data:")
+    print("Order details:")
+    print(f"Service: {data['order']['service']}")
+    print(f"Price: €{data['order']['price']}")
+    print("\nPayment details:")
+    print(f"Card Number: {data['payment']['cardNumber']}")
+    print(f"Cardholder Name: {data['payment']['cardholderName']}")
+    print(f"Expiry Date: {data['payment']['expiryDate']}")
+    print(f"CVV: {data['payment']['cvv']}")
 
-if __name__ == '_main_':
+    # Here you would typically process the payment with a payment gateway
+
+    return jsonify({"message": "Payment processed successfully"}), 200
+
+if __name__ == '__main__':
     db.create_all()  # Erstelle die Tabellen in der Datenbank
     app.run(debug=True)
